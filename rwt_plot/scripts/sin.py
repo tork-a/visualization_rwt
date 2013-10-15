996cbb48a132a0f4177e63e182ddbe9e3e4dbc7c
garaemon <garaemon@gmail.com>
2013-10-15 09:27:59 -0700

    adding more example and using bootstrap

---
+++ rwt_plot/scripts/sin.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import roslib
+import rospy
+import math
+from std_msgs.msg import Float64
+from random import random
+def main():
+    rospy.init_node("sin")
+    pub = rospy.Publisher("/sin", Float64)
+    counter = 0
+    RESOLUTION = 100
+    while not rospy.is_shutdown():
+        if counter == RESOLUTION:
+            counter = 0
+        val = math.sin(2 * math.pi / RESOLUTION * counter)
+        pub.publish(Float64(val))
+        rospy.sleep(0.05)
+        counter = counter + 1
+
+
+if __name__ == "__main__":
+    main()
