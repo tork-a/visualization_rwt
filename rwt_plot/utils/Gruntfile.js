f276590f68515820877c3d392c4160e96f83630a
garaemon <garaemon@gmail.com>
2013-10-17 21:38:29 -0700

    #2: adding Gruntfile.js which include concat and uglify task

---
+++ rwt_plot/utils/Gruntfile.js
@@ -0,0 +1,29 @@
+module.exports = function(grunt) {
+  var pkg = grunt.file.readJSON('package.json');
+  grunt.initConfig({
+    concat: {
+      build: {
+        src: ['../src/*.js'],
+        dest: '../www/rwt_plot.js'
+      }
+    },
+    uglify: {
+      options: {
+        report: 'min'
+      },
+      build: {
+        src: '../www/rwt_plot.js',
+        dest: '../www/rwt_plot.min.js'
+      }
+    }
+  });
+
+  // reading npm tasks
+  for(var taskName in pkg.devDependencies) {
+    if(taskName.substring(0, 6) == 'grunt-') {
+      grunt.loadNpmTasks(taskName);
+    }
+  }
+  
+  
+};
