164a909180a3df2fac557922bf75672e3d10ca6b
garaemon <garaemon@gmail.com>
2013-10-20 10:44:08 -0700

    #4: supporting ros

--- rwt_plot/www/examples/ros.js
+++ rwt_plot/www/examples/ros.js
@@ -1,30 +1,24 @@
 $(function() {
-    var plot = new ROSLIB.RWTPlot({
-        max_data: 50
-    });
+  var plot = new ROSLIB.RWTPlot({
+    max_data: 50
+  });
 
-    plot.initializePlot("#plot-area", {
-        yaxis: {
-            max: 100
-        }
-    });
+  plot.initializePlot("#plot-area", {
+    yaxis: {
+      max: 1.0
+    }
+  });
 
-    plot.draw();
-
-    // subscribe topic
-    var ros = new ROSLIB.Ros({
-        url: "ws://" + location.hostname + ":9090"
-    });
-    var sub = new ROSLIB.Topic({
-            ros: ros,
-      name: "/atlas/joint_states",
-      messageType: "sensor_msgs/JointState"
-            // name: "/random_float",
-            // messageType: "std_msgs/Float64"
-    });
-    sub.subscribe(function(msg) {
-      //plot.addData(msg.data);
-      plot.addData(msg.position[10]);
-        plot.draw();
-    });
+  // subscribe topic
+  var ros = new ROSLIB.Ros({
+    url: "ws://" + location.hostname + ":9090"
+  });
+  var sub = new ROSLIB.Topic({
+    ros: ros,
+    name: "/random_float",
+    messageType: "std_msgs/Float64"
+  });
+  sub.subscribe(function(msg) {
+    plot.addData(msg.data);
+  });
 });
