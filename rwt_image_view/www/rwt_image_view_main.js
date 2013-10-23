220130cc6ad8cafc2a7945a3c80096567b1ce48a
garaemon <garaemon@gmail.com>
2013-10-22 22:37:15 -0700

    #10: using topics_for_type to enumerate sensor_msgs/Image topics

--- rwt_image_view/www/rwt_image_view_main.js
+++ rwt_image_view/www/rwt_image_view_main.js
@@ -1,5 +1,19 @@
 $(function() {
 
+  ROSLIB.Ros.prototype.getTopicsForType = function(type, callback) {
+    var topicsForTypeClient = new ROSLIB.Service({
+      ros : this,
+      name : '/rosapi/topics_for_type',
+      serviceType : 'rosapi/TopicsForType'
+    });
+    var request = new ROSLIB.ServiceRequest({
+      type: type
+    });
+    topicsForTypeClient.callService(request, function(result) {
+      callback(result.topics);
+    });
+  };
+  
   ROSLIB.Ros.prototype.getTopicType = function(topic, callback) {
     var topicTypeClient = new ROSLIB.Service({
       ros : this,
@@ -19,51 +33,10 @@ $(function() {
     url: "ws://" + location.hostname + ":8888"
   });
 
-  function extractImageTopics(topics, callback, each_cb, prev_result) {
-    if (!_.isArray(prev_result)) {
-      prev_result = [];
-    }
-    if (topics.length === 0) {
-      callback(prev_result);
-    }
-    else {
-      isImageTopic(topics[0], function(result) {
-        each_cb(result, topics[0]);
-        if (result) {
-          extractImageTopics(topics.slice(1), callback, each_cb, prev_result.concat([topics[0]]));
-        }
-        else {
-          extractImageTopics(topics.slice(1), callback, each_cb, prev_result);
-        }
-      });
-    }
-  }
-  
-  function isImageTopic(topic, callback) {
-    ros.getTopicType(topic, function(type) {
-      if (type === 'sensor_msgs/Image') {
-        callback(true);
-      }
-      else {
-        callback(false);
-      }
-    });
-  };
-  
-  ros.getTopics(function(topics) {
-    var count = 0;
-    // we need to extract sensor_msgs/Image topics
-    extractImageTopics(topics, function(image_topics) {
-      image_topics.sort();
-      _.map(image_topics, function(topic) {
-        $("#topic-select").append('<option value="' + topic + '">' + topic + "</option>");
-      });
-      $('#overlay').remove();
-    }, function(result, topic) {
-      ++count;
-      $('#topic-progress-bar')
-        .attr('aria-valuenow', Math.ceil(count / topics.length * 100))
-        .css('width', Math.ceil(count / topics.length * 100) + '%');
+  ros.getTopicsForType('sensor_msgs/Image', function(image_topics) {
+    image_topics.sort();
+    _.map(image_topics, function(topic) {
+      $("#topic-select").append('<option value="' + topic + '">' + topic + "</option>");
     });
   });
 
