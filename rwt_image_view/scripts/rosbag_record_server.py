b0e60b6a2f5b397c15645cf96d43527f8279fdfb
jskuser <jskuser@start-laptop.(none)>
2013-10-29 19:41:08 +0900

    #14: calling rosbag, but it won't be killed

--- rwt_image_view/scripts/rosbag_record_server.py
+++ rwt_image_view/scripts/rosbag_record_server.py
@@ -3,18 +3,36 @@
 import rospy
 import roslib
 
+import os
+import subprocess
 roslib.load_manifest("rwt_image_view")
 
 from rwt_image_view.srv import *
+from std_srvs.srv import *
+import signal
+
+g_pobj = None
 
 def rosbagRecordRequestCB(req):
+    global g_pobj
     rospy.loginfo("topics: " + str(req.topics))
+    args = ["rosbag", "record", "-O", os.path.join(os.path.dirname(__file__), "..", "www", "tmp.bag")] + req.topics
+    g_pobj = subprocess.Popen(args)
     return RosbagRecordRequestResponse("foo")
-    
+
+def rosbagRecordStopCB(req):
+    global g_pobj
+    if g_pobj:
+        rospy.loginfo("sending SIGINT")
+        g_pobj.terminate()
+        #os.kill(g_pobj.pid, signal.SIGINT)
+        g_pobj = None
+    return EmptyResponse()
 
 def main():
     rospy.init_node("rosbag_record_server")
-    s = rospy.Service('rosbag_record', RosbagRecordRequest, rosbagRecordRequestCB)
+    s1 = rospy.Service('rosbag_record', RosbagRecordRequest, rosbagRecordRequestCB)
+    s2 = rospy.Service('rosbag_record_stop', Empty, rosbagRecordStopCB)
     rospy.spin()
 
 if __name__ == "__main__":
--- rwt_image_view/www/rwt_image_view_main.js
+++ rwt_image_view/www/rwt_image_view_main.js
@@ -41,6 +41,7 @@ $(function() {
   });
 
   var mjpeg_canvas = null;
+  var current_image_topic = null;
   $("#topic-form").submit(function(e) {
     if (mjpeg_canvas) {
       // remove the canvas here
@@ -51,6 +52,7 @@ $(function() {
     var topic = $("#topic-select").val();
     // first of all, subscribe the topic and detect the width/height
     var div_width = $("#canvas-area").width();
+    current_image_topic = topic;
     mjpeg_canvas = new MJPEGCANVAS.Viewer({
       divID : "canvas-area",
       host : location.hostname,
@@ -59,6 +61,62 @@ $(function() {
       height: 480 * div_width / 640.0
     });
     return false;
+  });
+
+  var recordingp = false;
+  $("#record-button").click(function(e) {
+    var $button = $(this);
+    e.preventDefault();
+    if (current_image_topic) {
+      if (!recordingp) {
+        var rosbagClient = new ROSLIB.Service({
+          ros : ros,
+          name : '/rosbag_record',
+          serviceType : 'rwt_image_view/RosbagRecordRequest'
+        });
+        var request = new ROSLIB.ServiceRequest({
+          topics: [current_image_topic]
+        });
+        rosbagClient.callService(request, function(result) {
+          recordingp = true;
+          $button.removeClass("btn-success")
+            .addClass("btn-danger")
+            .html("stop recording");
+          // download
+          
+        });
+      }
+      else {
+        recordingp = false;
+        var rosbagClient = new ROSLIB.Service({
+          ros : ros,
+          name : '/rosbag_record_stop',
+          serviceType : 'std_srvs/Empty'
+        });
+        var request = new ROSLIB.ServiceRequest({});
+        rosbagClient.callService(request, function(result) {
+          // download here
+          $.get({url: "/rwt_image_view/tmp.bag"}, function(content) {
+            var downloadAsFile = function(fileName) {
+              var blob = new Blob([content]);
+              var url = window.URL || window.webkitURL;
+              var blobURL = url.createObjectURL(blob);
+
+              var a = document.createElement('a');
+              a.download = fileName;
+              a.href = blobURL;
+              return a;
+            };
+            downloadAsFile("download.bag");
+            //var href = "data:application/octet-stream," + encodeURIComponent(content);
+            $button.removeClass("btn-danger")
+              .addClass("btn-success")
+              .html("record");  
+          });
+          
+        });
+      }
+    }
   });
   
 });
