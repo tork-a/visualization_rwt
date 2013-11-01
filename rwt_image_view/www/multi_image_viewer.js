bb991cb13b7eac17e2247d2140b0256cf0930939
jskuser <jskuser@start-laptop.(none)>
2013-11-01 16:23:25 +0900

    adding html and js for multi image viewer

---
+++ rwt_image_view/www/multi_image_viewer.js
@@ -0,0 +1,114 @@
+$(function() {
+
+  ROSLIB.Ros.prototype.getTopicsForType = function(type, callback) {
+    var topicsForTypeClient = new ROSLIB.Service({
+      ros : this,
+      name : '/rosapi/topics_for_type',
+      serviceType : 'rosapi/TopicsForType'
+    });
+    var request = new ROSLIB.ServiceRequest({
+      type: type
+    });
+    topicsForTypeClient.callService(request, function(result) {
+      callback(result.topics);
+    });
+  };
+  
+  ROSLIB.Ros.prototype.getTopicType = function(topic, callback) {
+    var topicTypeClient = new ROSLIB.Service({
+      ros : this,
+      name : '/rosapi/topic_type',
+      serviceType : 'rosapi/TopicType'
+    });
+    var request = new ROSLIB.ServiceRequest({
+      topic: topic
+    });
+    topicTypeClient.callService(request, function(result) {
+      callback(result.type);
+    });
+  };
+
+  
+  var ros = new ROSLIB.Ros({
+    url: "ws://" + location.hostname + ":8888"
+  });
+
+  ros.getTopicsForType('sensor_msgs/Image', function(image_topics) {
+    image_topics.sort();
+    _.map(image_topics, function(topic) {
+      $("#topic-select").append('<option value="' + topic + '">' + topic + "</option>");
+    });
+  });
+
+  var mjpeg_canvas = null;
+  var current_image_topic = null;
+  $("#topic-form").submit(function(e) {
+    if (mjpeg_canvas) {
+      // remove the canvas here
+      mjpeg_canvas = null;
+      $("#canvas-area canvas").remove();
+    }
+    e.preventDefault();
+    var topic = $("#topic-select").val();
+    // first of all, subscribe the topic and detect the width/height
+    var div_width = $("#canvas-area").width();
+    current_image_topic = topic;
+    mjpeg_canvas = new MJPEGCANVAS.Viewer({
+      divID : "canvas-area",
+      host : location.hostname,
+      topic : topic,
+      width: div_width,
+      height: 480 * div_width / 640.0
+    });
+    return false;
+  });
+
+  var recordingp = false;
+  $("#record-button").click(function(e) {
+    var $button = $(this);
+    e.preventDefault();
+    if (current_image_topic) {
+      if (!recordingp) {
+        var rosbagClient = new ROSLIB.Service({
+          ros : ros,
+          name : '/rosbag_record',
+          serviceType : 'rwt_image_view/RosbagRecordRequest'
+        });
+        var request = new ROSLIB.ServiceRequest({
+          topics: [current_image_topic]
+        });
+        rosbagClient.callService(request, function(result) {
+          recordingp = true;
+          $button.removeClass("btn-success")
+            .addClass("btn-danger")
+            .html("stop recording");
+          // download
+          
+        });
+      }
+      else {
+        recordingp = false;
+        var rosbagClient = new ROSLIB.Service({
+          ros : ros,
+          name : '/rosbag_record_stop',
+          serviceType : 'std_srvs/Empty'
+        });
+        var request = new ROSLIB.ServiceRequest({});
+        rosbagClient.callService(request, function(result) {
+          // download here
+          //var $alert = $('');
+          var html = '<div class="alert alert-info alert-dismissable" id="download-alert">\
+          <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>\
+          <a class="alert-link" href="/rwt_image_view/tmp.bag">download the bagfile from here via right-click</a>\
+</div>';
+          //$button.html('<a href="/rwt_image_view/tmp.bag">download</a>');
+          $("#topic-area").before(html);
+          $button.removeClass("btn-danger")
+            .addClass("btn-success")
+            .html("record");  
+        });
+      }
+    }
+  });
+  
+});
