339c17cf7ae2f8ca24b60d629b250f906d5149e0
garaemon <garaemon@gmail.com>
2014-01-02 04:08:51 +0900

    #15: implementing constructor of DiagnosticsStatus

--- rwt_robot_monitor/www/rwt_robot_monitor.js
+++ rwt_robot_monitor/www/rwt_robot_monitor.js
@@ -11,13 +11,31 @@
  * @param spec
  */
 ROSLIB.DiagnosticsStatus = function(spec) {
-  
+  if (typeof spec === 'undefined') {
+    spec = {};
+  }
+  this.name = spec.name;
+  this.message = spec.message;
+  this.hardware_id = spec.hardware_id;
+  this.values = {};
+  this.stamp = spec.timestamp;
+  for (var i = 0; i < spec.values.length; i++) {
+    this.values[spec.values[i].key] = spec.values[i].value;
+  }
 };
-
 
 ROSLIB.DiagnosticsStatus.createFromArray = function(msg) {
   var header = msg.header;
   var header_stamp = ROSLIB.Time.fromROSMsg(header);
+  return _.map(msg.status, function(status) {
+    return new ROSLIB.DiagnosticsStatus({
+      timestamp: header_stamp,
+      name: status.name,
+      message: status.message,
+      hardware_id: status.hardware_id,
+      values: status.values
+    });
+  });
 };
 
 // RobotMonitor.js
