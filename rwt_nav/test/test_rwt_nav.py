e728d5462e460b01e93ba25515d6cc7ac1c158fb
Kei Okada <k-okada@jsk.t.u-tokyo.ac.jp>
2021-10-24 16:28:50 +0900

    rwt_nav: add test/test_rwt_nav.py test/test_rwt_nav.test robot_pose_publihser.cpp

---
+++ rwt_nav/test/test_rwt_nav.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Software License Agreement (BSD License)
+#
+# Copyright (c) 2021, Kei Okada
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above
+#    copyright notice, this list of conditions and the following
+#    disclaimer in the documentation and/or other materials provided
+#    with the distribution.
+#  * Neither the name of the Copyright holder. nor the
+#    names of its contributors may be used to endorse or promote products
+#    derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+
+import sys
+import time
+import rospy
+import rostest
+import unittest
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
+
+from move_base_msgs.msg import MoveBaseActionGoal
+
+CLASSNAME = 'rwt_nav'
+
+class TestRwtNav(unittest.TestCase):
+
+    def goal_cb(self, msg):
+        rospy.logwarn("{} received".format(msg))
+        self.goal_msg = msg
+        self.goal_msg_received = self.goal_msg_received + 1
+
+    def __init__(self, *args):
+        super(TestRwtNav, self).__init__(*args)
+        rospy.init_node('test_rwt_nav')
+
+    def setUp(self):
+        self.goal_msg = None
+        self.goal_msg_received = 0
+
+        rospy.Subscriber('/move_base/goal', MoveBaseActionGoal, self.goal_cb)
+        self.url_base = rospy.get_param("url_roswww_testserver")
+
+        opts = webdriver.firefox.options.Options()
+        opts.add_argument('-headless')
+        self.browser = webdriver.Firefox(options=opts)
+
+        self.wait = webdriver.support.ui.WebDriverWait(self.browser, 10)
+        # maximize screen
+        self.browser.find_element_by_tag_name("html").send_keys(Keys.F11)
+
+    def tearDown(self):
+        try:
+            self.browser.close()
+            self.browser.quit()
+        except:
+            pass
+
+    def test_rwt_nav(self):
+        url = '%s/rwt_nav' % (self.url_base)
+        rospy.logwarn("Accessing to %s" % url)
+
+        self.browser.get(url)
+
+        # check settings
+        self.wait.until(EC.presence_of_element_located((By.ID, "button-ros-master-settings")))
+        settings = self.browser.find_element_by_id("button-ros-master-settings")
+        self.assertIsNotNone(settings, "Object id=button-ros-master-settings not found")
+        settings.click()
+
+        self.wait.until(EC.presence_of_element_located((By.ID, "input-ros-master-uri")))
+        uri = self.browser.find_element_by_id("input-ros-master-uri")
+        self.assertIsNotNone(uri, "Object id=input-ros-master-uri not found")
+        uri.clear();
+        uri.send_keys('ws://localhost:9090/')
+
+        self.wait.until(EC.presence_of_element_located((By.ID, "button-ros-master-connect")))
+        connect = self.browser.find_element_by_id("button-ros-master-connect")
+        self.assertIsNotNone(connect, "Object id=button-ros-master-connect")
+        connect.click()
+
+        # check image topic
+        self.wait.until(EC.presence_of_element_located((By.ID, "image-topic-select")))
+        image_topic = self.browser.find_element_by_id("image-topic-select")
+        self.assertIsNotNone(image_topic, "Object id=topic-select not found")
+        loop = 0
+        while image_topic.text == u'' and loop < 10:
+            loop = loop + 1
+            time.sleep(1)
+            image_topic = self.browser.find_element_by_id("image-topic-select")
+            self.assertIsNotNone(image_topic, "Object id=image-topic-select not found")
+        self.assertEqual(image_topic.text, u'/image_publisher/image_raw')
+
+        # wait for /map topic
+        self.wait.until(EC.presence_of_element_located((By.ID, "map")))
+        map = self.browser.find_element_by_id("map")
+        self.assertIsNotNone(map, "Object id=map not found")
+        map.click()
+
+        loop = 0
+        while loop < 20 and self.goal_msg == None:
+            loop = loop + 1
+            time.sleep(1)
+            map.click()
+
+        self.assertNotEqual(self.goal_msg, None)
+
+
+if __name__ == '__main__':
+    try:
+        rostest.run('rwt_nav', CLASSNAME, TestRwtNav, sys.argv)
+    except KeyboardInterrupt:
+        pass
+    print("{} exiting".format(CLASSNAME))
