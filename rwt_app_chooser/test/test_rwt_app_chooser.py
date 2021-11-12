b43c8f363739965eda51dacc9d51ac7e8e305e06
Kei Okada <k-okada@jsk.t.u-tokyo.ac.jp>
2021-11-12 10:14:32 +0900

    use xpath to get Hello World Task Icon

--- rwt_app_chooser/test/test_rwt_app_chooser.py
+++ rwt_app_chooser/test/test_rwt_app_chooser.py
@@ -131,30 +131,18 @@ class TestRwtAppChooser(unittest.TestCase):
         self.assertIsNotNone(select_robot, "Object div[class='item-content']/span[class='title']")
         self.assertTrue(u'pr1012' in select_robot.text)
         rospy.logwarn("Selected {} robot".format(select_robot.text))
-        
+
         self.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "li[class='item-expanded robot-list-item']")))
         select = self.browser.find_element_by_css_selector("li[class='item-expanded robot-list-item']")
         self.assertIsNotNone(select, "Object li[class='item-expanded robot-list-item']")
         select.click()
 
         # Select Task
-        self.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "span[class='Title']")))
-        task_texts = self.browser.find_elements_by_css_selector("span[class='Title']")
-        while len(task_texts) != 5:
-            task_texts = self.browser.find_elements_by_css_selector("span[class='Title']")
-            time.sleep(1)
-        task_text = task_texts[-1]
+        self.wait.until(EC.presence_of_element_located((By.XPATH, "//div/span[@class='Title' and contains(text(), 'Hello World')]")))
+        task_text = self.browser.find_element_by_xpath("//div/span[@class='Title' and contains(text(), 'Hello World')]")
         self.assertIsNotNone(task_text, "Object span[class='Title']")
         self.assertTrue(u'Hello World' in task_text.text, "Hello World is not found in {}".format(task_text))
-
-        self.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "div[class='column large-1 tablet-3 phone-6']")))
-        task_images = self.browser.find_elements_by_css_selector("div[class='column large-1 tablet-3 phone-6']")
-        while len(task_images) != 5:
-            task_images = self.browser.find_elements_by_css_selector("div[class='column large-1 tablet-3 phone-6']")
-            time.sleep(1)
-        task_image = task_images[-1]
-        self.assertIsNotNone(task_image, "Object div[class='column large-1 tablet-3 phone-6']")
-        task_image.click()
+        task_text.click()
         rospy.logwarn("Selected {} task".format(task_text.text))
 
         # input user name
